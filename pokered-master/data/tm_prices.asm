ad09f23c369a6273e1f727b4337a7e9772578f1c
Lewisk3 <korbin3lewis@gmail.com>
2019-10-22 04:04:07 -0700

    Initial commit

---
+++ pokered-master/data/tm_prices.asm
@@ -0,0 +1,29 @@
+TechnicalMachinePrices:
+; In thousands (nybbles).
+	dn 8, 7  ; TM_01, TM_02
+	dn 8, 6  ; TM_03, TM_04
+	dn 8, 9  ; TM_05, TM_06
+	dn 10, 9  ; TM_07, TM_08
+	dn 11, 12  ; TM_09, TM_10
+	dn 2, 9  ; TM_11, TM_12
+	dn 12, 13  ; TM_13, TM_14
+	dn 13, 13  ; TM_15, TM_16
+	dn 11, 10  ; TM_17, TM_18
+	dn 11, 10  ; TM_19, TM_20
+	dn 13, 13  ; TM_21, TM_22
+	dn 13, 10  ; TM_23, TM_24
+	dn 13, 12  ; TM_25, TM_26
+	dn 13, 10  ; TM_27, TM_28
+	dn 4, 9  ; TM_29, TM_30
+	dn 2, 9  ; TM_31, TM_32
+	dn 10, 10  ; TM_33, TM_34
+	dn 12, 10  ; TM_35, TM_36
+	dn 10, 13  ; TM_37, TM_38
+	dn 10, 12  ; TM_39, TM_40
+	dn 11, 10  ; TM_41, TM_42
+	dn 13, 10  ; TM_43, TM_44
+	dn 10, 12  ; TM_45, TM_46
+	dn 11, 12  ; TM_47, TM_48
+	dn 12, 10  ; TM_49, TM_50
+	dn 15, 0   ; TM_51, ???
+ 
